95dc52e59a7b408f244be6823746c172757c6e81
Ritesh Dubey <0xchikku@gmail.com>
2022-01-26 18:47:15 +0530

    todoapp files commit

--- ToDo/todoapp/todoapp/settings.py
+++ ToDo/todoapp/todoapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v7&isv()ben@6ya0uw@kdwn8vw-g01q&rw_dv8g631w%1sp1q1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','django-todo-ritesh.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
